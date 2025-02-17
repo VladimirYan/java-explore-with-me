12532da24adf71fd0c4908d2f26c48fe5fd45928
VladimirYan <167676012+VladimirYan@users.noreply.github.com>
2025-02-18 02:03:34 +0700

    Этап 1 ver 1.2

--- stat-service/stat-server/src/main/java/ru/practicum/statistic/api/model/StatisticEntity.java
+++ stat-service/stat-server/src/main/java/ru/practicum/statistic/api/model/StatisticEntity.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Timestamp;
+import java.util.Objects;
 
 @Entity
 @Table(name = "Statistics")
@@ -27,4 +28,21 @@ public class StatisticEntity {
 
     @Column(nullable = false)
     private Timestamp timestamp;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StatisticEntity that = (StatisticEntity) o;
+        return Objects.equals(app, that.app) &&
+                Objects.equals(ip, that.ip) &&
+                Objects.equals(uri, that.uri) &&
+                Objects.equals(timestamp, that.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(app, ip, uri, timestamp);
+    }
+
 }
